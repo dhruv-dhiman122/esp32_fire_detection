2ee57bbf749035df3d91f79d4fa390ba1106d5f9
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-04 17:57:43 +0530

    Added the code for seeing if the input is enabled or not

--- main/fire_detection.c
+++ main/fire_detection.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "driver/gpio.h"
+
+//space for macro used for coding
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
 
 // space for defining the pins
 
-#define MQ5_SENSOR GPIO32
+#define MQ5_SENSOR GPIO_NUM_32
 
 // space for configing the pins
 
 gpio_config_t MQ5_PIN_CONFIG = {
-    .pin_bit_mask = (1ULL << 32),
+    .pin_bit_mask = (1ULL << MQ5_SENSOR),
     .mode = GPIO_MODE_INPUT,
     .pull_up_en = GPIO_PULLUP_DISABLE,
     .pull_down_en = GPIO_PULLDOWN_DISABLE,
@@ -18,4 +24,14 @@ gpio_config_t MQ5_PIN_CONFIG = {
 void app_main(void)
 {
     gpio_config(&MQ5_PIN_CONFIG);
+
+    int mq5_pin_input_enable = gpio_input_enable(MQ5_SENSOR);
+    if(mq5_pin_input_enable == 0) {
+        printf("The input is enabled for the pin of MQ5 sensor\n");
+    }
+    else {
+        fprintf("The input is not enabled for the pin of MQ5 sensor\n");
+        exit(EXT_FAILURE);
+    }
+
 }
